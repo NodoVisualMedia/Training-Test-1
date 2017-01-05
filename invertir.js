9c46d6119b5017d8a8115cf636a27d11b4878ad7
santi <santiago.pchon@nodovm.com>
2017-01-05 00:47:43 -0500

    project completed

--- invertir.js
+++ invertir.js
@@ -1,31 +1,27 @@
+var nombre = document.getElementById("nombre");
+var sexo = document.getElementById("sexo");
+var palabra = document.getElementById("navidad");
+var resultado = document.getElementById("resultado");
 
-	//seccion1//
- 	var Nombre = document.getElementById('Nombre');
-	var Sexo = document.getElementById('Sexo');
- 	var Palabra = document.getElementById('Palabra');
- 	var Send = document.getElementById('Send');
- 
- 	//seccion2
- 	var nombres = document.getElementById('nombres');
- 	var sexos = document.getElementById('sexos');
- 	var navis = document.getElementById('navis');
- 	var invertida = "";
- 	var enviar = function (argument) {
- 		nombres.innerHTML = Nombre.value;
- 		sexos.innerHTML =   Sexo.value;
- 
- 		var x = Palabra.value.length;
- 	
- 		while (x>=0) {
- 			invertida += Palabra.value.charAt(x);
- 			x--;
- 		}
- 	
- 		navis.innerHTML = invertida;
- 	}
- 
- 	Send.addEventListener("click", enviar);
- 	
+function llenar() {
+	
+	var invertir = "";
+	var x = palabra.value.length;
+	var cadenaInvertida = "";
+
+	while (x>=0) {
+		cadenaInvertida = cadenaInvertida + palabra.value.charAt(x);
+		x--;
+	}
+
+	console.log(cadenaInvertida);
+
+	resultado.innerHTML = " \
+	Nombre: <center>"+nombre.value+"</center><br> \
+	<br>Sexo: <center>"+sexo.value+"</center><br> \
+	<br>Palabra Navideña: <center>"+cadenaInvertida;
+}
+
  
  	
  
